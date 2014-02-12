8f3e18cbb117ec503c7bab47d6f9be1f84d477f8
fwilson <fwilson@fwilson.me>
2014-02-12 13:22:39 -0500

    let's try github issues now

---
+++ modules/gh-issues.js
@@ -0,0 +1,17 @@
+var gh = require('github');
+
+module.exports.register = function(bot) {
+    var issuere = /.*([a-zA-Z0-9]+)\/([a-zA-Z0-9])#([0-9]+).*/
+    bot.on('message#', function(nick, chan, text) {
+        var github = new gh({version: "3.0.0"});
+        var matches = text.match(issuere);
+        if(matches !== null){
+            var user = matches[0];
+            var repo = matches[1];
+            var issue = parseInt(matches[2]);
+            github.issues.getRepoIssue({user: user, repo: repo, number: issue}, function(e, r){
+                console.log(JSON.stringify(r));
+            });
+        }
+    }
+}
