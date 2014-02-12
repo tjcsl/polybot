a31dbb36671ee5561118495efa53be8e0a3c7046
fwilson <fwilson@fwilson.me>
2014-02-12 13:00:49 -0500

    maybe fix url.js

--- modules/url.js
+++ modules/url.js
@@ -1,10 +1,31 @@
+var cheerio = require('cheerio');
+var http = require('http');
+
+function download(url, callback) {
+    http.get(url, function(res) {
+        var data = "";
+        res.on('data', function (chunk) {
+            data += chunk;
+        });
+        res.on("end", function() {
+            callback(data);
+        });
+    }).on("error", function() {
+        callback(null);
+    });
+}
+
 module.exports.register = function(bot) {
     var urlre = /https?:\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/gi
     bot.on("message#", function(nick, chan, text) {
         var urls = text.match(urlre);
         if(!urls) return;
         urls.forEach(function(f) {
-            bot.say(chan, "Wow, a URL! " + f);
+            download(f, function(data) {
+                page = cheerio.load(data);
+                var title = page('title').text()
+                bot.say(chan, "** " + title)
+            });
         });
     });
 }
