19fae69a85f888534d6328c231e828f8a5884423
fwilson <fwilson@fwilson.me>
2014-02-17 14:21:40 -0500

    actually add op

--- commands/op.js
+++ commands/op.js
@@ -5,7 +5,7 @@
 module.exports.commands = [
     {
         name: "op",
-        nArgs: 0
+        nArgs: 0,
         permission: "op",
         callback: function(reply, data, args) {
             data.bot.send("MODE", data.chan, "+o", data.sender);
