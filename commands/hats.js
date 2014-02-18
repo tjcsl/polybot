a17e615aca059e1efd7ae2a2e9baf689d1519092
fwilson <fwilson@fwilson.me>
2014-02-17 19:19:12 -0500

    op -> hats, add voice

--- commands/hats.js
+++ commands/hats.js
@@ -10,5 +10,13 @@ module.exports.commands = [
         callback: function(reply, data, args) {
             data.bot.send("MODE", data.chan, "+o", data.sender);
         }
+    },
+    {
+        name: "voice",
+        nArgs: 0,
+        permission: "voice",
+        callback: function(reply, data, args) {
+            data.bot.send("MODE", data.chan, "+v", data.sender);
+        }
     }
 ];
