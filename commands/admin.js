473c7b76bea37e720f29fe116ada88620fc530d8
fwilson <fwilson@fwilson.me>
2014-02-18 15:58:43 -0500

    admin command module

---
+++ commands/admin.js
@@ -0,0 +1,28 @@
+modules.exports.commands = [
+    {
+        name: "admin",
+        permission: "admin",
+        callback: function(reply, data, args) {
+            // This is a "multi-command"
+            var cmd = args[0];
+            args = args.slice(1);
+            switch(args[0]) {
+                case "nick":
+                    data.bot.send("NICK", args[1]);
+                    break;
+                case "die":
+                    data.bot.send("QUIT", "Killed by an admin.");
+                    break;
+                case "join":
+                    args.forEach(function(c){data.bot.join(c);});
+                    break;
+                case "part":
+                    args.forEach(function(c){data.bot.part(c);});
+                    break;
+                default:
+                    reply("What?");
+                    break;
+            }
+        }
+    }
+];
