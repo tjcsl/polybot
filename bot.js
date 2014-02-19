991d5acd4588d5dc45ed2af84b954a862bbb861c
fwilson <fwilson@fwilson.me>
2014-02-19 15:39:19 -0500

    add registered notice

--- bot.js
+++ bot.js
@@ -31,6 +31,10 @@ bot.db = db;
 bot.config = config;
 bot.modules = {};
 
+bot.on("registered", function(message) {
+    console.log("We have liftoff! Connected to the IRC server.");
+});
+
 bot.on("error", function(message) {
     console.warn(message);
 });
