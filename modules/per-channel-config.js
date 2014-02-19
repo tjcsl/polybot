26da8871de914947bacd18eeabfe223e6681e1fc
fwilson <fwilson@fwilson.me>
2014-02-19 18:05:49 -0500

    fix per-channel-config

--- modules/per-channel-config.js
+++ modules/per-channel-config.js
@@ -2,7 +2,15 @@ module.exports.register = function(bot) {
     bot.channelConfig = {};
     bot.on('join', function(channel, nick) {
         if(nick != bot.config.nick) return;
-        if(channel in bot.channelConfig) return;
-        bot.channelConfig[channel] = bot.config.defaultChannelConfig;
-    }
+        if(channel in bot.channelConfig) {
+            for(key in bot.config.defaultChannelConfig) {
+                if (!(key in bot[channel])) {
+                    bot[channel][key] = bot.config.defaultChannelConfig[key];
+                }
+            }
+        }
+        else {
+            bot.channelConfig[channel] = bot.config.defaultChannelConfig;
+        }
+    });
 }
