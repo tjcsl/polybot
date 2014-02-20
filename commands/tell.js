46e9f9782c6e51ef235c71385e24bc9b9df709cb
fwilson <fwilson@fwilson.me>
2014-02-19 21:46:45 -0500

    add 'tell.js'

---
+++ commands/tell.js
@@ -0,0 +1,12 @@
+module.exports.commands = [
+    {
+        name: "tell",
+        permission: "admin",
+        callback: function(reply, data, args) {
+            var dest = args[0];
+            var text = args.slice(1).join(" ");
+            data.bot.say(dest, text);
+            reply("OK, I told " + dest + ".");
+        }        
+    }
+];
