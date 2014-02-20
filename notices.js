b93b5fe435e42e5961345c86f093a9f6f3e623f4
fwilson <fwilson@fwilson.me>
2014-02-20 08:36:36 -0500

    ...how did _that_ get there.

--- notices.js
+++
@@ -1,7 +0,0 @@
-bot.on("registered", function(message) {
-    console.log("We have liftoff! Connected to the IRC server.");
-});
-
-bot.on("error", function(message) {
-    console.warn(message);
-});
