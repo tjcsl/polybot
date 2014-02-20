5072e74ad96356ae6b394d885a2d96da3285c1ed
fwilson <fwilson@fwilson.me>
2014-02-19 20:23:45 -0500

    whoopsie.

--- modules/per-channel-config.js
+++ modules/per-channel-config.js
@@ -17,19 +17,17 @@ module.exports.register = function(bot, dontAttachListeners) {
             bot.channelConfig[data.channel][data.key] = doParseValue(data.value);
         });
     });
-    if(!dontAttachListeners){
-        bot.on('join', function(channel, nick) {
-            if(nick != bot.config.nick) return;
-            if(channel in bot.channelConfig) {
-                for(key in bot.config.defaultChannelConfig) {
-                    if (!(key in bot[channel])) {
-                        bot[channel][key] = bot.config.defaultChannelConfig[key];
-                    }
+    bot.on('join', function(channel, nick) {
+        if(nick != bot.config.nick) return;
+        if(channel in bot.channelConfig) {
+            for(key in bot.config.defaultChannelConfig) {
+                if (!(key in bot[channel])) {
+                    bot[channel][key] = bot.config.defaultChannelConfig[key];
                 }
             }
-            else {
-                bot.channelConfig[channel] = bot.config.defaultChannelConfig;
-            }
-        });
-    }
+        }
+        else {
+            bot.channelConfig[channel] = bot.config.defaultChannelConfig;
+        }
+    });
 }
