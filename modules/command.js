8898dfe1a7a58b152549d3add87be2270a70ecbc
fwilson <fwilson@fwilson.me>
2014-03-04 13:00:39 -0500

    fixing stuff with command

--- modules/command.js
+++ modules/command.js
@@ -19,7 +19,7 @@ function doChaining(commands, text, data, message) {
         console.log("args: " + match);
 
         if(command.permission) return; // commands that require permission
-                                       // can't be done because I'm lazy
+        // can't be done because I'm lazy
 
         var reply = function(t) {
             text = text.replace(old, t);
@@ -30,6 +30,48 @@ function doChaining(commands, text, data, message) {
     return text;
 }
 
+function cmdhandler(nick, to, text, message) {
+    var reply_dest = (to == bot.config.nick ? nick : to);
+    var reply = function(t) { bot.say(reply_dest, t); }
+    var data = {bot: bot, sender: nick, chan: to, msg: message};
+    if(text.indexOf(bot.config.nick + ": ") == 0) text = text.replace(bot.config.nick + ": ", "");
+    else if(text.indexOf(bot.config.nick + ", ") == 0) text = text.replace(bot.config.nick + ", ", "");
+    else if(to == bot.config.nick) text = text;
+    else if(text.indexOf(bot.config.cmdchar) == 0 && bot.channelConfig[to].globalCmdcharAllowed) text = text.replace(bot.config.cmdchar, "");
+    else if(text.indexOf(bot.channelConfig[to].cmdchar) == 0 && bot.channelConfig[to].cmdcharAllowed) text = text.replace(bot.channelConfig[to].cmdchar, "");
+    else return;
+    // text = doChaining(commands, text, data, message);
+    text = text.split(" ");
+    if(Object.keys(commands).indexOf(text[0]) != -1) {
+        var cmd = commands[text[0]];
+        if(cmd.permission){
+            bot.hasAccess(message.user, message.host, to, cmd.permission, function(a) {
+                if(!a) {
+                    bot.say(nick, "You don't have the '" + cmd.permission + "' permission.");
+                }
+                else {
+                    text = text.slice(1);
+                    try {
+                        cmd.callback(reply, data, text);
+                    }
+                    catch(e) {
+                        reply(e);
+                    }
+                }
+            });
+        }
+        else {
+            try {
+                cmd.callback(reply, data, text.slice(1));
+            }
+            catch(e) {
+                reply(e);
+            }
+        }
+    }
+};
+
+module.exports.cmdhandler = cmdhandler;
 module.exports.register = function(bot) {
     var command_pkgs = {};
     var commands = {};
@@ -47,44 +89,5 @@ module.exports.register = function(bot) {
         });
         console.log("done loading command package " + f);
     });
-    bot.on('message', function(nick, to, text, message) {
-        var reply_dest = (to == bot.config.nick ? nick : to);
-        var reply = function(t) { bot.say(reply_dest, t); }
-        var data = {bot: bot, sender: nick, chan: to, msg: message};
-        if(text.indexOf(bot.config.nick + ": ") == 0) text = text.replace(bot.config.nick + ": ", "");
-        else if(text.indexOf(bot.config.nick + ", ") == 0) text = text.replace(bot.config.nick + ", ", "");
-        else if(to == bot.config.nick) text = text;
-        else if(text.indexOf(bot.config.cmdchar) == 0 && bot.channelConfig[to].globalCmdcharAllowed) text = text.replace(bot.config.cmdchar, "");
-        else if(text.indexOf(bot.channelConfig[to].cmdchar) == 0 && bot.channelConfig[to].cmdcharAllowed) text = text.replace(bot.channelConfig[to].cmdchar, "");
-        else return;
-        // text = doChaining(commands, text, data, message);
-        text = text.split(" ");
-        if(Object.keys(commands).indexOf(text[0]) != -1) {
-            var cmd = commands[text[0]];
-            if(cmd.permission){
-                bot.hasAccess(message.user, message.host, to, cmd.permission, function(a) {
-                    if(!a) {
-                        bot.say(nick, "You don't have the '" + cmd.permission + "' permission.");
-                    }
-                    else {
-                        text = text.slice(1);
-                        try {
-                            cmd.callback(reply, data, text);
-                        }
-                        catch(e) {
-                            reply(e);
-                        }
-                    }
-                });
-            }
-            else {
-                try {
-                    cmd.callback(reply, data, text.slice(1));
-                }
-                catch(e) {
-                    reply(e);
-                }
-            }
-        }
-    });
+    bot.on('message', cmdhandler);
 };
