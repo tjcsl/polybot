c7754ad6094414ab9bd5f1523f29c432b45ad341
fwilson <fwilson@fwilson.me>
2014-03-05 11:54:29 -0500

    maybe fix commandS

--- modules/command.js
+++ modules/command.js
@@ -30,6 +30,12 @@ function doChaining(commands, text, data, message) {
     return text;
 }
 
+
+
+module.exports.register = function(bot) {
+    var command_pkgs = {};
+    var commands = {};
+
 function cmdhandler(nick, to, text, message) {
     var reply_dest = (to == bot.config.nick ? nick : to);
     var reply = function(t) { bot.say(reply_dest, t); }
@@ -74,10 +80,7 @@ function cmdhandler(nick, to, text, message) {
     }
 };
 
-module.exports.cmdhandler = cmdhandler;
-module.exports.register = function(bot) {
-    var command_pkgs = {};
-    var commands = {};
+
     require('fs').readdirSync('./commands/').forEach(function(f){
         f = f.replace('.js', '');
         console.log("loading command package " + f);
