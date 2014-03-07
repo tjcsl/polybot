5e95c4592c221e86abe1b2fcdaf27cac8fc4c1ab
fwilson <fwilson@fwilson.me>
2014-03-06 21:22:37 -0500

    maybe fix that

--- modules/command.js
+++ modules/command.js
@@ -31,69 +31,68 @@ function doChaining(commands, text, data, message) {
 }
 
 
-
 module.exports.register = function(bot) {
     var command_pkgs = {};
     var commands = {};
 
-function cmdhandler(nick, to, text, message) {
-    var reply_dest = (to == bot.config.nick ? nick : to);
-    var reply = function(t) { bot.say(reply_dest, t); }
-    var data = {bot: bot, sender: nick, chan: to, msg: message};
-    if(text.indexOf(bot.config.nick + ": ") == 0) text = text.replace(bot.config.nick + ": ", "");
-    else if(text.indexOf(bot.config.nick + ", ") == 0) text = text.replace(bot.config.nick + ", ", "");
-    else if(to == bot.config.nick) text = text;
-    else if(bot.channelConfig[to]){
-        if (text.indexOf(bot.config.cmdchar) == 0 && bot.channelConfig[to].globalCmdcharAllowed) text = text.replace(bot.config.cmdchar, "");
-        else if(text.indexOf(bot.channelConfig[to].cmdchar) == 0 && bot.channelConfig[to].cmdcharAllowed) text = text.replace(bot.channelConfig[to].cmdchar, "");
+    function cmdhandler(nick, to, text, message) {
+        var reply_dest = (to == bot.config.nick ? nick : to);
+        var reply = function(t) { bot.say(reply_dest, t); }
+        var data = {bot: bot, sender: nick, chan: to, msg: message};
+        if(text.indexOf(bot.config.nick + ": ") == 0) text = text.replace(bot.config.nick + ": ", "");
+        else if(text.indexOf(bot.config.nick + ", ") == 0) text = text.replace(bot.config.nick + ", ", "");
+        else if(to == bot.config.nick) text = text;
+        else if(bot.channelConfig[to]){
+            if (text.indexOf(bot.config.cmdchar) == 0 && bot.channelConfig[to].globalCmdcharAllowed) text = text.replace(bot.config.cmdchar, "");
+            else if(text.indexOf(bot.channelConfig[to].cmdchar) == 0 && bot.channelConfig[to].cmdcharAllowed) text = text.replace(bot.channelConfig[to].cmdchar, "");
+            else return;
+        }
         else return;
-    }
-    else return;
-    // text = doChaining(commands, text, data, message);
-    text = text.split(" ");
-    if(Object.keys(commands).indexOf(text[0]) != -1) {
-        var cmd = commands[text[0]];
-        if(cmd.permission){
-            bot.hasAccess(message.user, message.host, to, cmd.permission, function(a) {
-                if(!a) {
-                    bot.say(nick, "You don't have the '" + cmd.permission + "' permission.");
+        // text = doChaining(commands, text, data, message);
+        text = text.split(" ");
+        if(Object.keys(commands).indexOf(text[0]) != -1) {
+            var cmd = commands[text[0]];
+            if(cmd.permission){
+                bot.hasAccess(message.user, message.host, to, cmd.permission, function(a) {
+                    if(!a) {
+                        bot.say(nick, "You don't have the '" + cmd.permission + "' permission.");
+                    }
+                    else {
+                        text = text.slice(1);
+                        try {
+                            cmd.callback(reply, data, text);
+                        }
+                        catch(e) {
+                            reply(e);
+                        }
+                    }
+                });
+            }
+            else {
+                try {
+                    cmd.callback(reply, data, text.slice(1));
                 }
-                else {
-                    text = text.slice(1);
-                    try {
-                        cmd.callback(reply, data, text);
-                    }
-                    catch(e) {
-                        reply(e);
-                    }
+                catch(e) {
+                    reply(e);
+                }
+            }
+        }
+        };
+
+
+        require('fs').readdirSync('./commands/').forEach(function(f){
+            f = f.replace('.js', '');
+            console.log("loading command package " + f);
+            command_pkgs[f] = require('../commands/' + f).commands;
+            command_pkgs[f].forEach(function(cmd) {
+                if(Object.keys(commands).indexOf(cmd.name) == -1){
+                    commands[cmd.name] = cmd;
+                }
+                else{
+                    console.log("warning: command " + cmd.name + " from " + f + " is already loaded in a different package");
                 }
             });
-        }
-        else {
-            try {
-                cmd.callback(reply, data, text.slice(1));
-            }
-            catch(e) {
-                reply(e);
-            }
-        }
-    }
-};
-
-
-    require('fs').readdirSync('./commands/').forEach(function(f){
-        f = f.replace('.js', '');
-        console.log("loading command package " + f);
-        command_pkgs[f] = require('../commands/' + f).commands;
-        command_pkgs[f].forEach(function(cmd) {
-            if(Object.keys(commands).indexOf(cmd.name) == -1){
-                commands[cmd.name] = cmd;
-            }
-            else{
-                console.log("warning: command " + cmd.name + " from " + f + " is already loaded in a different package");
-            }
+            console.log("done loading command package " + f);
         });
-        console.log("done loading command package " + f);
-    });
-    bot.on('message', cmdhandler);
-};
+        bot.on('message', cmdhandler);
+    };
