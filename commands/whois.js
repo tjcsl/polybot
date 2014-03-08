78b041f28c1152171b8c8eaea6c65718e44d08ad
fwilson <fwilson@fwilson.me>
2014-03-08 11:05:03 -0500

    fix hostmask

--- commands/whois.js
+++ commands/whois.js
@@ -3,7 +3,7 @@ module.exports.commands = [
         name: "hostmask",
         callback: function(reply, data, args) {
             data.bot.whois(args[0], function(result) {
-                reply(result.username + "@" + result.host);
+                reply(result.user + "@" + result.host);
             });
         }
     }
