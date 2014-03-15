4dd7d445542f0cec23853542ffa3995237be58a3
fwilson <fwilson@fwilson.me>
2014-03-15 12:01:42 -0400

    initial commit of help

---
+++ commands/help.js
@@ -0,0 +1,15 @@
+module.exports.commands = [
+    {
+        name: "help",
+        callback: function(reply, data, args) {
+            if(!args[0]) {
+                reply("Loaded command packages: " + Object.keys(data.bot.commands).join(", "));
+            }
+            else {
+                if(!args[1]) {
+                    reply("Commands in package " + args[1] + ": " + Object.keys[data.bot.commands[args[1]].join(", "));
+                }
+            }
+        }
+    }
+];
