89a9828b50b4018658652a175465032b017f84e6
fwilson <fwilson@fwilson.me>
2014-03-16 11:34:10 -0400

    chanperms

---
+++ modules/chanperms.js
@@ -0,0 +1,12 @@
+modules.exports.register = function(bot) {
+    bot.channelUsers = {}
+
+    bot.on('join',  function(chan) { bot.send('NAMES', chan); });
+    bot.on('part',  function(chan) { bot.send('NAMES', chan); });
+    bot.on('+mode', function(chan) { bot.send('NAMES', chan); });
+    bot.on('-mode', function(chan) { bot.send('NAMES', chan); });
+
+    bot.on('names', function(chan, nicks) {
+        bot.channelUsers[chan] = nicks;
+    });
+};
