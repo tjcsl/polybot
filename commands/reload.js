5070506ec3b9cc48ef8fe87a5fc425f2d3e6c4ce
fwilson <fwilson@fwilson.me>
2014-03-17 11:53:25 -0400

    inb4 i syntax error again

---
+++ commands/reload.js
@@ -0,0 +1,10 @@
+module.exports.commands = [
+    {
+        name: "reload",
+        permission: "admin",
+        callback: function(reply, data, args) {
+            data.bot.removeListener('message', data.bot.cmdhandler);
+            data.bot.modules.command.register(data.bot);
+        }
+    }
+];
