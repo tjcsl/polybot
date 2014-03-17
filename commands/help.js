0146883f35947351fb8bb1ef5d09610517df43bd
fwilson <fwilson@fwilson.me>
2014-03-17 14:16:59 -0400

    right.

--- commands/help.js
+++ commands/help.js
@@ -3,11 +3,11 @@ module.exports.commands = [
         name: "help",
         callback: function(reply, data, args) {
             if(!args[0]) {
-                reply("Loaded command packages: " + Object.keys(data.bot.commands).join(", "));
+                reply("Loaded command packages: " + Object.keys(data.bot.command_pkgs).join(", "));
             }
             else {
                 if(!args[1]) {
-                    reply("Commands in package " + args[1] + ": " + Object.keys(data.bot.commands[args[1]].join(", ")));
+                    reply("Commands in package " + args[1] + ": " + Object.keys(data.bot.command_pkgs[args[1]].join(", ")));
                 }
             }
         }
