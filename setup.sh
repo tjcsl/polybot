0a3e82e474efddfd8b0ca143b4a3c80a3a61ee7d
fwilson <fwilson@fwilson.me>
2014-03-17 11:48:48 -0400

    experimental setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+echo "#!/bin/sh" >> start.sh
+echo "What's the name of your postgres DB? "
+read pgname
+echo "export DATABASE_URL=\"postgres://127.0.0.1/${pgname}\"" >> start.sh
+echo "node bot" >> start.sh
+echo "Creating tables...."
+psql -d $pgname < sql/default.sql
+echo "Done!"
